de1c8af3ca4afbcc55b6b1bb32f24ce5e085b486
bellarm <rachelitabella@rocketmail.com>
2017-01-06 00:26:15 +0700

    try dfs but havent fully worked

--- sudoku.c
+++ sudoku.c
@@ -28,194 +28,269 @@
 
 
 typedef struct _grid {
-	char num;
-	int given;
+    char num;
+    int given;
 } grid;
 
-void printPuzzle(grid box[ROW][COL+1]);
+typedef grid * Grid;
+void printPuzzle(void);
 void printLine(void);
-void solveSudoku(grid box[ROW][COL+1]);
-int compareCol (char a, int col, grid box[ROW][COL+1]);
-int compareRow (char a, int row, grid box[ROW][COL+1]);
-int compareReg (char a, int col, int row, grid box[ROW][COL+1]);
+void solveSudoku(void);
+int compareCol (char a, int col);
+int compareRow (char a, int row);
+int compareReg (char a, int col, int row);
+int new(int row, int col);
+
+grid box[ROW][COL+1] = {0};
 
 int main (int argc, char * argv[]) {
 
-	grid box[ROW][COL+1] = {0};
-	//The [COL+1] is to acount new line ('\n')
+    //The [COL+1] is to acount new line ('\n')
 
-	int col = 0; // the puzzle is stored as a 9x9 box
-	int row = 0; 
+    int col = 0; // the puzzle is stored as a 9x9 box
+    int row = 0; 
 
-	while(row < ROW) {
-		
-		col = 0;
-		while (col < COL+1) {
-			box[row][col].num = getchar();
-			if(box[row][col].num != '\n'){
-				assert(box[row][col].num >= '0' && 
-						box[row][col].num <= '9');
-			}
-			if(box[row][col].num != '0'){
-				box[row][col].given = TRUE;
-			} else {
-				box[row][col].given = FALSE;
-			}
-			col++;
-		}
-		row++;
-	}
-	//put terminating character at the end of the array
-	box[ROW-1][COL].num = 0;
+    while(row < ROW) {
+        
+        col = 0;
+        while (col < COL+1) {
+            box[row][col].num = getchar();
+            if(box[row][col].num != '\n'){
+                assert(box[row][col].num >= '0' && 
+                        box[row][col].num <= '9');
+            }
+            if(box[row][col].num != '0'){
+                box[row][col].given = TRUE;
+            } else {
+                box[row][col].given = FALSE;
+            }
+            col++;
+        }
+        row++;
+    }
+    //put terminating character at the end of the array
+    box[ROW-1][COL].num = 0;
 
-	printPuzzle(box);
-	solveSudoku(box);
+    if (new(0,0)) {
+        printPuzzle();
+    } else {
+        printf("puzzle could not be solved\n");
+        printPuzzle();
+    }
 
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
-void printPuzzle(grid box[ROW][COL+1]){
+void printPuzzle(void){
 
-	int col = 0; 
-	int row = 0;
-	while(row < ROW) {
-		col = 0;
-		if(row % REG_ROW == 0) {
-			printLine();
-		}
-		while (col < COL) {
-			if (col % REG_ROW == 0) {
-				printf("| ");
-			}
-			if (box[row][col].given == TRUE){
-				printf("\x1b[1;33m"); //print in bold
-			}
+    int col = 0; 
+    int row = 0;
+    while(row < ROW) {
+        col = 0;
+        if(row % REG_ROW == 0) {
+            printLine();
+        }
+        while (col < COL) {
+            if (col % REG_ROW == 0) {
+                printf("| ");
+            }
+            if (box[row][col].given == TRUE){
+                printf("\x1b[1;33m"); //print in bold
+            }
 
-			printf("%c ", box[row][col].num); //actual number inside the box
-			printf("\x1b[0m");
-			col++;
-		}			
-		printf("|\n");
-		row++;
-	}
-	printLine();
+            printf("%c ", box[row][col].num); //actual number inside the box
+            printf("\x1b[0m");
+            col++;
+        }           
+        printf("|\n");
+        row++;
+    }
+    printLine();
 }
 
 void printLine(void){
-	int counter = 0;
-	while (counter < LINE) {
-		if (counter % 8 == 0){
-			printf("+");
-		} else {
-			printf("-");
-		}
-	counter++;
-	}
-	printf("\n");
+    int counter = 0;
+    while (counter < LINE) {
+        if (counter % 8 == 0){
+            printf("+");
+        } else {
+            printf("-");
+        }
+    counter++;
+    }
+    printf("\n");
 }
 
 
 // Solve the puzzle
-void solveSudoku(grid box[ROW][COL+1]){
-	int row = 0;
-	int col = 0;
-	char a = '1';
-	while (row < ROW && a != ':') {
-		col = 0;
-		while (col < COL && a != ':') {
-			if (box[row][col].num == '0') {
-				a = '1';
-				while(box[row][col].num == '0'){
-					if(compareCol(a, col, box) == TRUE &&
-						compareRow(a, row, box) == TRUE &&
-						compareReg(a, col, row, box) == TRUE){
-						box[row][col].num = a;
-					} else {
-						a++;
-					}
-				}
-			}
-			col++;
-		}
-		row++; 
-		
-	}
-	if (a == ':') {
-		printf("Puzzle can not be solved\n");
-	} else {
-		printPuzzle(box);
-	}
+void solveSudoku(void){
+    int row = 0;
+    int col = 0;
+    char a = '1';
+    while (row < ROW && a != ':') {
+        col = 0;
+        while (col < COL && a != ':') {
+            if (box[row][col].num == '0') {
+                a = '1';
+                while(box[row][col].num == '0'){
+                    if(compareCol(a, col) == TRUE &&
+                        compareRow(a, row) == TRUE &&
+                        compareReg(a, col, row) == TRUE){
+                        box[row][col].num = a;
+                    } else {
+                        a++;
+                    }
+                }
+            }
+            col++;
+        }
+        row++; 
+        
+    }
+    if (a == ':') {
+        printf("Puzzle can not be solved\n");
+    } else {
+        printPuzzle();
+    }
 
 }
 
-int compareCol (char a, int col, grid box[ROW][COL+1]){
-	int i = 0;
-	int j = 0;
+int compareCol (char a, int col){
+    int i = 0;
+    int j = 0;
 
-	while (i < ROW){
-		if (a == box[i][col].num){
-			j++;
-		}
-		i++;
-	}
+    while (i < ROW){
+        if (a == box[i][col].num){
+            j++;
+        }
+        i++;
+    }
 
-	if(j > 0){
-		i = FALSE;
-	} else {
-		i = TRUE;
-	}
-	return i;
+    if(j > 0){
+        i = FALSE;
+    } else {
+        i = TRUE;
+    }
+    return i;
 }
 
-int compareRow (char a, int row, grid box[ROW][COL+1]){
-	int i = 0;
-	int j = 0;
-
-	while (i < COL){
-		if (a == box[row][i].num){
-			j++;
-		}
-		i++;
-	}
-
-	if(j > 0){
-		i = FALSE;
-	} else {
-		i = TRUE;
-	}
-	return i;
+int compareRow (char a, int row){
+    int i = 0;
+    int j = 0;
+    printPuzzle();
+    while (i < COL){
+        if (a == box[row][i].num){
+            j++;
+        }
+        printf(" %d,%d -> %c\n", row, i, box[row][i].num);
+        i++;
+    }
+    printf("\n");
+    if(j > 0){
+        i = FALSE;
+    } else {
+        i = TRUE;
+    }
+    return i;
 }
 
-int compareReg (char a, int col, int row, grid box[ROW][COL+1]){
-	row = row / 3;
-	col = col / 3;
+int compareReg (char a, int col, int row){
 
-	int i = 3*row;
-	int j = 3*col;
-	int colF = j;
-	row = i + 3;
-	col = j + 3;
+    row = row / 3;
+    col = col / 3;
+    int i = 3*row;
+    int j = 3*col;
+    int colF = j;
+    row = i + 3;
+    col = j + 3;
 
-	int k = 0;
-	int l = 0;
+    int k = 0;
+    int l = 0;
+    //printf("%d-%d, %d-%d\n", i,row, j, col);
+    while (i < row){
+        j = colF;
+        while (j < col){
+            if (a == box[i][j].num){
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
 
-	while (i < row){
-		j = colF;
-		while (j < col){
-			if (a == box[i][j].num){
-				k++;
-			}
-			j++;
-		}
-		i++;
-	}
+    if(k > 0){
+        l = FALSE;
+    } else {
+        l = TRUE;
+    }
+    return l;
+}
 
-	if(k > 0){
-		l = FALSE;
-	} else {
-		l = TRUE;
-	}
-	return l;
+// NEW ATTEMPT
+/*  Find all the possible numbers for a grid
+    Then do a DFS
+    DFS:
+        - if puzzle solved: return 1
+        - else do a recursion:
+            - check if a number is legal
+            - if legal: call the next one
+            - if next returns 0:
+                - try another number
+*/
+
+int new(int row, int col){
+    // First check if puzzle solved
+    if (row > 8) {
+        return 1;
+    }
+    // Then check if grid is empty
+    int next_row, next_col;
+    while (box[row][col].given == TRUE) {
+        if (col < 8) {
+            next_row = row;
+            next_col = col + 1;
+        } else {
+            next_row = row + 1;
+            next_col = 0;
+        }
+        row = next_row;
+        col = next_col;
+    }
+    if (col < 8) {
+        next_row = row;
+        next_col = col + 1;
+    } else {
+        next_row = row + 1;
+        next_col = 0;
+    }
+    char a = '1';
+    int result = 0;
+    printf("%d %d\n", row, col);
+    while (TRUE) {
+
+        if (row == 3 && col == 8 && a == '5') {
+                printf("\t%c\n", a);
+                printf("%d\n", compareCol(a, col));
+                printf("%d\n", compareRow(a, row));
+                printf("%d\n", compareReg(a, col, row));
+        }
+        if (compareCol(a, col) == TRUE &&
+           compareRow(a, row) == TRUE &&
+           compareReg(a, col, row) == TRUE) {
+            box[row][col].num = a;
+            
+            result = new(next_row, next_col);
+            if (result) {
+                return 1;
+            }
+        }    
+        a++;
+        if (a == ':' && !result) {
+            return 0;
+        } else if (a == ':') {
+            a = '1';
+        }
+    }
+    return 0;
 }
---
+++ sudoku.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.5
+import sys
+
+""" SUDOKU
+    will probably need:
+        - THE GRID -> 9x9 array?
+        - INITIALISATION Function
+        - Solve
+"""
+class Sudoku(object):
+    def __init__(self):
+        self.box = None
+    def solve(self):
+        pass
